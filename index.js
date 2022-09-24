5fb9a4d6e7138c33ce8d6b1cd4e86c3be5954a5e
Aditya Agarwal <agarwaladitya381@gmail.com>
2022-09-25 01:06:59 +0530

    documenting comments

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const json = require("./jsonCollection.js");
-const map = require("./mapCollection.js");
-module.exports = { json, map };
+/**
+ * @file Collection package
+ * @name Collection
+ * @version 2.0.4
+ * @copyright BeastBoyADI © 2022
+ * @license Apache-2.0
+ */
+
+/**
+ * @exports @beastboyadi/collection
+ * @description Exporting collection module
+ * @example
+ * //Importing the module
+ * const {json, map} = require("@beastboyadi/collection");
+ * //Initiating jsonCollection class
+ * let jsonCol = new json();
+ * //Initiating mapCollection class
+ * let mapCol = new map();
+ */
+module.exports = {
+  json: require("./jsonCollection.js"),
+  map: require("./mapCollection.js")
+}
